80221c51917a6f95c0c0ae81f01b53d2ab27342c
andreahuang <hyfandrea@gmail.com>
2015-02-01 16:29:39 +0800

    Moved fortune arrays to lib

--- meadowlark.js
+++ meadowlark.js
@@ -1,21 +1,21 @@
+var fortune = require('./lib/fortune.js');
 var express = require('express');
 var handlebars=require('express3-handlebars').create({defaultLayout:'main'});
+
 var app = express();
 app.engine('handlebars',handlebars.engine);
 app.set('view engine','handlebars');
 app.set('port',process.env.PORT||3000);
 app.use(express.static(__dirname +'/public'));
 
-var fortunes=["Conquer your fears or they will conquer you.","Rivers need springs.",
-"Do not fear what you don't know.","You will have a pleasant surprise.",
-"Whener possible, keep it simple."];
+
 
 app.get('/',function(req,res){
   res.render('home');
 });
 app.get('/about*',function(req,res){
-  var randomFortune=fortunes[Math.floor(Math.random()*fortunes.length)];
-  res.render('about',{fortune:randomFortune});
+
+  res.render('about',{fortune:fortune.getFortune()});
 });
 app.use(function(req,res,next){
   res.status(404);
